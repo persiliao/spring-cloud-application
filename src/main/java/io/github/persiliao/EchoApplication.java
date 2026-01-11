9e0510eca69c5bd4be50e368f583f9d34918375e
persiliao <xiangchu.liao@gmail.com>
2026-01-11 21:23:02 +0800

    refactor: optimize code

---
+++ src/main/java/io/github/persiliao/EchoApplication.java
@@ -0,0 +1,17 @@
+package io.github.persiliao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Persi.Liao
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EchoApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(EchoApplication.class, args);
+    }
+}
